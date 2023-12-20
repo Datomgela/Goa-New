5c40fce613405fe8eb3bc9f95be12bb4d131b373
Datomgela <mgeladzedato184@gmail.com>
2023-12-20 20:01:56 +0400

    first commit

---
+++ visual studio code test/python/tester 2.py
@@ -0,0 +1,9 @@
+#print (5 == 5)
+#print (5 == 7)
+#print (5 != 7)
+#print (5 != 5)
+#print (5 <= 5)
+#print (5 >= 5)
+password = "SecretWord"
+guess = "1234" 
+print(guess != password)
---
+++ visual studio code test/python/tester 3.py
@@ -0,0 +1,29 @@
+import collections
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+class FrenchDeck:
+ ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+ suits = 'spades diamonds clubs hearts'.split()
+ def __init__(self):
+    self._cards = [Card(rank, suit) for suit in self.suits
+ for rank in self.ranks]
+ def __len__(self):
+    return len(self._cards)
+ def __getitem__(self, position):
+    return self._cards[position]
+beer_card = Card('7', 'diamonds')
+beer_card
+Card(rank='7', suit='diamonds')
+deck = FrenchDeck()
+len(deck)
+52
+deck[0]
+Card(rank='2', suit='spades')
+deck[-1]
+Card(rank='A', suit='hearts')
+from random import choice
+choice(deck)
+Card(rank='3', suit='hearts')
+choice(deck)
+Card(rank='K', suit='spades')
+choice(deck)
+Card(rank='2', suit='clubs')
