ba55be357713c199c22d5806dd0758bfa3ecd5bc
Datomgela <138392124+Datomgela@users.noreply.github.com>
2023-12-20 20:49:38 +0400

    Delete visual studio code test directory

--- visual studio code test/python/tester 2.py
+++
@@ -1,9 +0,0 @@
-#print (5 == 5)
-#print (5 == 7)
-#print (5 != 7)
-#print (5 != 5)
-#print (5 <= 5)
-#print (5 >= 5)
-password = "SecretWord"
-guess = "1234" 
-print(guess != password)
--- visual studio code test/python/tester 4.py
+++
@@ -1,8 +0,0 @@
-first = {1, 2, 3, 4, 5, 6}
-second = { 6, 7, 8, 9}
-
-print(first | second)
-print(first & second)
-print(first - second)
-print(second - first)
-print(first ^ second)
